c0812e7d2bc35c65a86d2076d0045ba9a83ab318
ArturHaavisto <arturhaavisto1@gmail.com>
2022-05-17 13:19:39 +0300

    style: change dark theme
    Added new colors and changed dark theme.

--- App/app/src/main/java/fi/tuni/tamk/tiko/haavisto/jokeapp/ui/theme/Color.kt
+++ App/app/src/main/java/fi/tuni/tamk/tiko/haavisto/jokeapp/ui/theme/Color.kt
@@ -6,3 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val DarkBlue = Color(0xFF274B69)
+val MediumBlue = Color(0xFF3F4D63)
+val LightBlue = Color(0xFF85A1C1)
+val BaseGray = Color(0xFFC6CCD8)
+val BaseBlack = Color(0xFF202022)
+
+
